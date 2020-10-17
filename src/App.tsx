b2e0c2a38dd314057d23453795005e132a362946
Vitaly Baum <vitaliy.baum@yandex.ru>
2020-10-17 23:18:38 +0700

    add hot loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { hot } from 'react-hot-loader/root';
 import React from 'react';
 
 const App = () => (
@@ -6,4 +7,4 @@ const App = () => (
   </div>
 );
 
-export default App;
+export default hot(App);
