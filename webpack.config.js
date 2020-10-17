ea71753da2d3c3c2de4458a3c558879c1adfde18
Vitaly Baum <vitaliy.baum@yandex.ru>
2020-10-17 23:16:21 +0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: '[name].[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.([jt])sx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  devServer: {
+    port: 3000,
+    compress: true,
+    publicPath: '/',
+    hot: true,
+    host: 'dota-puff.ru',
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      inject: true,
+      cache: false,
+    }),
+  ],
+};
